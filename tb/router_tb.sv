b82eb8dec02cea1b88d59cfded782ccd88762464
Søren <s245638@dtu.dk>
2026-08-25 10:12:38 +0200

    for the router I adding registers, clock and connected all inputs to the east output

--- tb/router_tb.sv
+++ tb/router_tb.sv
@@ -1,38 +1,86 @@
 `timescale 1ns/1ps
 
 module router_tb ;
+  logic clk;
+  logic rst_n;
+  logic[3:0] location;
 
-  logic[36:0] local_in;
-  logic[36:0] south_in;
   logic[36:0] east_in;
   logic[36:0] west_in;
+  logic[36:0] north_in;
+  logic[36:0] south_in;
+  logic[36:0] local_in;
 
+  logic[36:0] east_out;
+  logic[36:0] west_out;
   logic[36:0] north_out;
+  logic[36:0] south_out;
+  logic[36:0] local_out;
 
 
     router dut(
-        .local_in(local_in),
-        .south_in(south_in),
+        .clk(clk),
+        .rst_n(rst_n),
+        .location(location),
+
         .east_in(east_in),
         .west_in(west_in),
-        .north_out(north_out)
+        .north_in(north_in),
+        .south_in(south_in),
+        .local_in(local_in),
+
+        .east_out(east_out),
+        .west_out(west_out),
+        .north_out(north_out),
+        .south_out(south_out),
+        .local_out(local_out)
     );
 
     initial begin
-        local_in = 36'd0;
-        east_in = 36'd1;
-        south_in = 36'd2;
-        west_in = 36'd3;
+        //restarting the module
+        rst_n = 0;
+        clk = 0;
+        #10;
+        rst_n = 1;
+
+        //The location of the testing router
+        location = 4'b1010;
+
+        local_in = 37'b0000000000000000000000000000000000000;
+        clk = 1;
+        #1;
+        clk = 0;
+        $display("local_in=%b, east_out=%b", local_in[36:30], east_out[36:30]);
 
         #10;
-        $display("east_in=%b, north_out=%b", east_in, north_out);
-
-        east_in = 36'd2;
+        local_in = 37'b0001100000000000000000000000000000000;
+        clk = 1;
+        #1;
+        clk = 0;
+        $display("local_in=%b, east_out=%b", local_in[36:30], east_out[36:30]);
 
         #10;
-        $display("east_in=%b, north_out=%b", east_in, north_out);
+        local_in = 37'b1001100000000000000000000000000000000;
+        clk = 1;
+        #1;
+        clk = 0;
+        $display("local_in=%b, east_out=%b", local_in[36:30], east_out[36:30]);
 
-        #10;
+        #10
+        local_in = 37'b1000100000000000000000000000000000000;
+        clk = 1;
+        #1;
+        clk = 0;
+        $display("local_in=%b, east_out=%b", local_in[36:30], east_out[36:30]);
+
+        #10
+        local_in = 37'b1111100000000000000000000000000000000;
+        clk = 1;
+        #1;
+        clk = 0;
+        $display("local_in=%b, east_out=%b", local_in[36:30], east_out[36:30]);
+
+
         $finish;
     end
 
