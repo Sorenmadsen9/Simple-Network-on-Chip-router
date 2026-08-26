82ec68b6e599dc68716c31e5bd84411707fa33b4
Jeffrey Arboe <jeffrey@sikker.eu>
2026-08-26 12:10:04 +0200

    delt sætninger i 2

--- src/router.sv
+++ src/router.sv
@@ -67,43 +67,59 @@ module router (
 
       //  ***CHOOSING WHERE THE PACKAGES SHOULD MOVE***
       // determining which package moves east
-      if(west_in[36] == 1'b1 && west_in[35:34] != 2'b00 && west_in[33:32] > location[1:0]) begin
+      if(west_in[36] == 1'b1 && west_in[35:34] != 2'b00
+       && west_in[33:32] > location[1:0]) begin
         east_reg <= west_in;
-      end else if(north_in[36] == 1'b1 && north_in[35:34] != 2'b00 && north_in[33:32] > location[1:0]) begin
+      end else if(north_in[36] == 1'b1 && north_in[35:34] != 2'b00
+       && north_in[33:32] > location[1:0]) begin
         east_reg <= north_in;
-      end else if(south_in[36] == 1'b1 && south_in[35:34] != 2'b00 && south_in[33:32] > location[1:0]) begin
+      end else if(south_in[36] == 1'b1 && south_in[35:34] != 2'b00
+       && south_in[33:32] > location[1:0]) begin
         east_reg <= south_in;
-      end else if(local_in[36] == 1'b1 && local_in[35:34] != 2'b00 && local_in[33:32] > location[1:0]) begin
+      end else if(local_in[36] == 1'b1 && local_in[35:34] != 2'b00
+       && local_in[33:32] > location[1:0]) begin
         east_reg <= local_in;
       end
       // determining which package moves west
-      if(east_in[36] == 1'b1 && east_in[35:34] != 2'b00 && east_in[33:32] < location[1:0] && east_in[33:32] != 2'b00) begin
+      if(east_in[36] == 1'b1 && east_in[35:34] != 2'b00
+       && east_in[33:32] < location[1:0] && east_in[33:32] != 2'b00) begin
         west_reg <= east_in;
-      end else if(north_in[36] == 1'b1 && north_in[35:34] != 2'b00 && north_in[33:32] < location[1:0] && north_in[33:32] != 2'b00) begin
+      end else if(north_in[36] == 1'b1 && north_in[35:34] != 2'b00
+       && north_in[33:32] < location[1:0] && north_in[33:32] != 2'b00) begin
         west_reg <= north_in;
-      end else if(south_in[36] == 1'b1 && south_in[35:34] != 2'b00 && south_in[33:32] < location[1:0] && south_in[33:32] != 2'b00) begin
+      end else if(south_in[36] == 1'b1 && south_in[35:34] != 2'b00
+       && south_in[33:32] < location[1:0] && south_in[33:32] != 2'b00) begin
         west_reg <= south_in;
-      end else if(local_in[36] == 1'b1 && local_in[35:34] != 2'b00 && local_in[33:32] < location[1:0] && local_in[33:32] != 2'b00) begin
+      end else if(local_in[36] == 1'b1 && local_in[35:34] != 2'b00
+       && local_in[33:32] < location[1:0] && local_in[33:32] != 2'b00) begin
         west_reg <= local_in;
       end
       // determining which package moves north
-      if(east_in[36] == 1'b1 && east_in[33:32] == location[1:0] && east_in[35:34] < location[3:2] && east_in[35:34] != 2'b00) begin
+      if(east_in[36] == 1'b1 && east_in[33:32] == location[1:0]
+       && east_in[35:34] < location[3:2] && east_in[35:34] != 2'b00) begin
         north_reg <= east_in;
-      end else if(west_in[36] == 1'b1 && west_in[33:32] == location[1:0] && west_in[35:34] < location[3:2] && west_in[35:34] != 2'b00) begin
+      end else if(west_in[36] == 1'b1 && west_in[33:32] == location[1:0]
+       && west_in[35:34] < location[3:2] && west_in[35:34] != 2'b00) begin
         north_reg <= west_in;
-      end else if(south_in[36] == 1'b1 && south_in[33:32] == location[1:0] && south_in[35:34] < location[3:2] && south_in[35:34] != 2'b00) begin
+      end else if(south_in[36] == 1'b1 && south_in[33:32] == location[1:0]
+       && south_in[35:34] < location[3:2] && south_in[35:34] != 2'b00) begin
         north_reg <= south_in;
-      end else if(local_in[36] == 1'b1 && local_in[33:32] == location[1:0] && local_in[35:34] < location[3:2] && local_in[35:34] != 2'b00) begin
+      end else if(local_in[36] == 1'b1 && local_in[33:32] == location[1:0]
+       && local_in[35:34] < location[3:2] && local_in[35:34] != 2'b00) begin
         north_reg <= local_in;
       end
       // determining which package moves south
-      if(east_in[36] == 1'b1 && east_in[33:32] == location[1:0] && east_in[35:34] > location[3:2]) begin
+      if(east_in[36] == 1'b1 && east_in[33:32] == location[1:0]
+       && east_in[35:34] > location[3:2]) begin
         south_reg <= east_in;
-      end else if(west_in[36] == 1'b1 && west_in[33:32] == location[1:0] && west_in[35:34] > location[3:2]) begin
+      end else if(west_in[36] == 1'b1 && west_in[33:32] == location[1:0]
+       && west_in[35:34] > location[3:2]) begin
         south_reg <= west_in;
-      end else if(north_in[36] == 1'b1 && north_in[33:32] == location[1:0] && north_in[35:34] > location[3:2]) begin
+      end else if(north_in[36] == 1'b1 && north_in[33:32] == location[1:0]
+       && north_in[35:34] > location[3:2]) begin
         south_reg <= north_in;
-      end else if(local_in[36] == 1'b1 && local_in[33:32] == location[1:0] && local_in[35:34] > location[3:2]) begin
+      end else if(local_in[36] == 1'b1 && local_in[33:32] == location[1:0]
+       && local_in[35:34] > location[3:2]) begin
         south_reg <= local_in;
       end
       // determining which package moves local
